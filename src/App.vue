703c2b38e4a2813e8eb907659d8d1720b18054ce
JoseMa Santos <josantos@domingoalonsogroup.com>
2023-09-04 08:46:52 +0200

    ✨ add state

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import TheHeader from "@/components/TheHeader.vue";
+import ProductCard from "@/components/ProductCard.vue";
+//import products from "@/data/products.json";
+
+
+import { useProductStore } from "@/stores/ProductStores";
+
+const productStore = useProductStore()
+
+/*import { storeToRefs } from "pinia";
+const {products} = storeToRefs(useProductStore())*/
+
+</script>
+
+<template>
+  <div class="container">
+    <TheHeader />
+    <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
+      <ProductCard
+        v-for="product in productStore.products"
+        :key="product.name"
+        :product="product"
+      />
+    </ul>
+  </div>
+</template>
